2e230dcb7f06e0effa5bf00782b4fcca63c11b26
nikhillohra <nikhillohra99@gmail.com>
2024-04-12 17:38:52 +0530

    api

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
 //4bca902a
-const API_URL = "http://www.omdbapi.com/?apikey=4bca902a";
+const API_URL = "https://www.omdbapi.com/?apikey=4bca902a";
 
 const App = () => {
   const [movies, setMovies] = useState();
